0371b2e44b4b0b654fa5536397d0ecc430765a90
fabfankhauser <145201828+fabfankhauser@users.noreply.github.com>
2023-09-27 17:04:53 +0200

    Add files via upload
    FILE ADDED

---
+++ FirstScript.R
@@ -0,0 +1,23 @@
+r <- 4.34
+  
+u <- r*2*pi
+ul = paste(u)
+print(paste("The circumfrence is:", ul))
+
+Subjects <- data.frame(Name = c("Hans", "Jürgen", "Peter", "Isabella", "Greta"), Age = c(64,34,44,23,28), Nicotineaddiction = c(TRUE,TRUE,FALSE,FALSE,TRUE))
+print(Subjects)
+
+Addicts <- Subjects[c(TRUE), ]
+print(Addicts)
+
+x <- 7878   
+x <- x + 1
+y <- x
+x <- 2 * x
+x <- x + 4
+save(x, file = "~/Proseminar.Geoprocessing/Tryout/x")
+x <- x / 2
+xfinal = x - y
+print(paste(x, ",",xfinal))
+
+                                    
